43db839898994edb6a4b0bc06f85b2374a9e96d2
mcbigballs <paionnonionn@gmail.com>
2024-06-17 19:50:32 +0300

    aaas

--- src/core/ngx_proxy_protocol.c
+++ src/core/ngx_proxy_protocol.c
@@ -103,7 +103,7 @@ typedef struct {
     ngx_uint_t                              type;
 } ngx_proxy_protocol_tlv_entry_t;
 
-
+static u_char *ngx_proxy_protocol_v2_write(ngx_connection_t *c, u_char *buf, u_char *last);
 static u_char *ngx_proxy_protocol_read_addr(ngx_connection_t *c, u_char *p,
     u_char *last, ngx_str_t *addr);
 static u_char *ngx_proxy_protocol_read_port(u_char *p, u_char *last,
@@ -315,7 +315,7 @@ ngx_proxy_protocol_read_port(u_char *p, u_char *last, in_port_t *port,
 
 
 u_char *
-ngx_proxy_protocol_write(ngx_connection_t *c, u_char *buf, u_char *last, ngx_uint_t pp_version)
+ngx_proxy_protocol_write(ngx_connection_t *c, u_char *buf, u_char *last)
 {
     ngx_uint_t  port, lport;
 
--- src/core/ngx_proxy_protocol.h
+++ src/core/ngx_proxy_protocol.h
@@ -30,7 +30,7 @@ struct ngx_proxy_protocol_s {
 u_char *ngx_proxy_protocol_read(ngx_connection_t *c, u_char *buf,
     u_char *last);
 u_char *ngx_proxy_protocol_write(ngx_connection_t *c, u_char *buf,
-    u_char *last);
+    u_char *last, ngx_uint_t pp_version);
 ngx_int_t ngx_proxy_protocol_get_tlv(ngx_connection_t *c, ngx_str_t *name,
     ngx_str_t *value);
 
